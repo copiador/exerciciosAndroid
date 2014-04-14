c1635d1f288a84f4d50e424ee896247d34690b43
copiador <jessemarquesads@gmail.com>
2014-04-14 16:15:28 -0300

    primeiro git android

---
+++ PrimeiroExercicioJesseMarques/src/br/com/example/primeiroexerciciojessemarques/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.example.primeiroexerciciojessemarques;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.linear_layout_main);
+
+		
+	}
+
+	
+
+}
